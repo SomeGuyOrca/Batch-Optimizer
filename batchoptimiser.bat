bdeacdf87e975816ce14321358d49d86ae6af607
SomeGuyOrca <96922863+SomeGuyOrca@users.noreply.github.com>
2022-11-20 10:27:06 +0530

    Update batchoptimiser.bat

--- batchoptimiser.bat
+++ batchoptimiser.bat
@@ -7,7 +7,7 @@ echo.
 echo.
 echo.
 
-echo                                       [ 1 ] Power plan          [ 3 ] Removing useless files
+echo                                       [ 1 ] Power plan          [ 3 ] Removing temporary files
 echo. 
 echo                                       [ 2 ] Error checking      [ 4 ] Disabling background apps 
 
@@ -18,7 +18,7 @@ echo                                      { Type close to Exit }
 set /p optimization= 
  
 if %optimization% == close exit 
-if %optimization% == 1 powercfg -duplicatescheme 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+if %optimization% == 1 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 echo. 
 pause
 cls
