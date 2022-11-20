d0d19da6ba38fcbbe9d0b63e7134699f09217de1
SomeGuyOrca <96922863+SomeGuyOrca@users.noreply.github.com>
2022-11-20 10:35:15 +0530

    Update batchoptimiser.bat

--- batchoptimiser.bat
+++ batchoptimiser.bat
@@ -15,23 +15,32 @@ echo.
 echo                                      { Type close to Exit } 
 
 
-set /p optimization= 
+set /p optimisation=>
  
-if %optimization% == close exit 
-if %optimization% == 1 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+if %optimisation% == close exit 
+if %optimisation% == 1 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 echo. 
 pause
 cls
 goto tweaks
-if %optimization% == 2 DISM /Online /Cleanup-Image /ScanHealth
+cls 
+pause
+goto tweaks
+if %optimisation% == 2 DISM /Online /Cleanup-Image /ScanHealth
 pause
 cls
 goto tweaks
-if %optimization% == 3 del /q/f/s %TEMP%\* 
+cls
+pause
+goto tweaks
+if %optimisation% == 3 del /q/f/s %TEMP%\* 
 pause
 cls
 goto tweaks
-if %optimization% == 4 Reg Add HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications /v GlobalUserDisabled /t REG_DWORD /d 1 /f
+cls
+pause
+goto tweaks
+if %optimisation% == 4 Reg Add HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications /v GlobalUserDisabled /t REG_DWORD /d 1 /f
 pause 
 cls
 goto tweaks   
